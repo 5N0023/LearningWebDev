6bc866f99847effe800170cc7070d4bd51326687
snooze <snooze@ubuntu.myguest.virtualbox.org>
2023-03-19 22:58:00 +0000

    some edits

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function() {
+    $('.ajax-link').click(function(event) {
+      event.preventDefault();
+  
+      var url = $(this).attr('href');
+  
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'html',
+        success: function(data) {
+          $('#content').html(data);
+        }
+      });
+    });
+  });
